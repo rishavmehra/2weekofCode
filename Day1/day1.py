1d59668a4ff6e9678fee0f42b2496923aa532792
rishavmehra <rishavmehra61@gmail.com>
2022-01-16 01:22:15 +0530

    My First Day

---
+++ Day1/day1.py
@@ -0,0 +1,48 @@
+#Day1
+# print("Hello World")
+#Day 1 Excercise 1
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this")
+# print("print('what to print')")
+# print("Strings:")
+# print("Hello World!\nHello World!\nHello World")
+#Concatenation
+# print("Hello"+" Rishu")
+# print("hello"+"rishu!")
+# fixed The error
+# print("Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+# print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n."))
+
+# print("Hello"+input("what is your name")+"!")
+#input() will get user Input in Console
+#The print() will print the world "Hello" and the user input
+
+
+# name="Angela"
+# print(len(name))
+#variable name is Angela
+#print(len) return the lenght of the variable "name"
+# print(len(input("enter world to be calculcated:")))
+
+# Variables
+# name = input("what is your name (this program for calculate the lenght):")
+# lenght= len(name)
+# print(lenght)
+
+# exercise Veriable
+# a=input("a:")
+# b=input("b:")
+# c=a
+# a=b
+# b=c
+# print("a:"+a)
+# print("b:"+b)
+
+#band name generator
+
+
+city=input("What's the name of city you grew in?\n")
+pet=input("What's the name of your pet?\n")
+print("your band name could be "+city+" "+pet)
