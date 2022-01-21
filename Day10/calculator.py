868b5681fe1b6682e3ff6ef538c0ee611f903f21
rishavmehra <rishavmehra61@gmail.com>
2022-01-22 00:55:19 +0530

    Push/Update

---
+++ Day10/calculator.py
@@ -0,0 +1,41 @@
+
+from ast import operator
+from art import logo
+print(logo)
+
+
+def add(n1,n2):
+    return n1+n2
+def subtract(n1,n2):
+    return n1-n2
+def multiply(n1,n2):
+    return n1*n2
+def division(n1,n2):
+    return n1/n2
+
+cal_dic={
+    "+":add,
+    "-":subtract,
+    "*":multiply,
+    "/":division,
+}
+def calculator():
+    num1=int(input("What's the First Number: "))
+    for symbol in cal_dic:
+        print(symbol)
+    should_continue=True
+
+    while should_continue:
+        operation_symbol=input("Pick An Operations: ")
+        num2=int(input("What's the next Number: "))
+        cal_function=cal_dic[operation_symbol]  
+        answer=cal_function(num1,num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n'  to start new calculation ")=="y":
+            num1=answer
+        else:
+            should_continue=False
+            calculator()
+calculator()
---
+++ Day10/rough.py
@@ -0,0 +1,7 @@
+#Function with Outputs
+def format_name(f_name,l_name):
+    formated_f_name=f_name.title()
+    formated_l_name=l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+
+print(format_name(input("First Name: "),input("Last Name: ")))
